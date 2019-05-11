063020b028757e21e30db9aae5cf03a3b576cab2
Craig Miller <craigmiller160@gmail.com>
2019-05-11 14:04:38 -0400

    Refactored the run script

--- run.sh
+++ run.sh
@@ -2,8 +2,16 @@
 
 function help {
     echo "Available Docker Applications"
+    echo "  postgres = The Postgresql database and PGAdmin tool"
     echo ""
-    echo "postgres = The Postgresql database and PGAdmin tool"
+    echo "Options"
+    echo "  build = Build the app, don't start it"
+    echo "  start = Start the app. Will build it if it has not already been built."
+    echo ""
+    echo "Example Command"
+    echo "  bash run.sh postgres start"
+    echo "Other"
+    echo "  help = The help menu"
 }
 
 function run_app {
@@ -11,20 +19,34 @@ function run_app {
     sudo -E docker-compose up -d
 }
 
-if [[ $# -ne 1 ]]; then
-    echo "Error! Invalid number of arguments"
+function build_app {
+	sudo -E docker-compose build
+}
+
+function build_or_run {
+	case $1 in
+		"build") build_app ;;
+		"start") run_app ;;
+		*)
+			echo "Error! Invalid command: $1. Try using the 'help' option for more details"
+		;;
+	esac
+}
+
+if [[ $# -ne 2 ]]; then
+    echo "Error! Invalid number of arguments. Please see the help menu"
 fi
 
 case $1 in
     "postgres")
         cd Postgres
-        run_app
+        build_or_run $2
     ;;
     "help")
         help
     ;;
     *)
-        echo "Error! Invalid input."
+        echo "Error! Invalid app name: $1. Try using the 'help' option for more details"
         exit 1
     ;;
 esac
