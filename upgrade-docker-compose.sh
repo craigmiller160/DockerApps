adcf383a29d7e90bbe163f71ccf65b21d39eefdc
Craig Miller <craigmiller160@gmail.com>
2019-06-16 13:38:59 -0400

    Added upgrade docker-compose script

---
+++ upgrade-docker-compose.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+dpkg -s jq 1>/dev/null 2>/dev/null
+if [[ $? -ne 0 ]]; then
+	echo "Installing JQ package"
+	sudo apt install jq
+else
+	echo "JQ package already installed"
+fi
+
+
+
+compose_version=$(curl https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
+echo "Latest Docker-Compose Version: $compose_version"
+output="/usr/local/bin/docker-compose"
+echo "Downloading and installing Docker-Compose"
+sudo curl -L https://github.com/docker/compose/releases/download/$compose_version/docker-compose-$(uname -s)-$(uname -m) -o $output
+sudo chmod +x $output
+echo "Installed Docker-Compose Version: $(docker-compose --version)"
