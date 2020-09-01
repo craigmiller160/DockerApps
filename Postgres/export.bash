0830a709c8b2ac9eb856b4f91cc18b5937d512b0
Craig Miller <craigmiller160@gmail.com>
2020-09-01 08:34:03 -0400

    Updated export script for k8s

--- Postgres/export.bash
+++ Postgres/export.bash
@@ -5,4 +5,8 @@ if [[ $# -lt 1 ]]; then
 	exit 1
 fi
 
-sudo docker exec postgres pg_dump "$1" -U user --column-inserts --data-only
+#sudo docker exec postgres pg_dump "$1" -U user --column-inserts --data-only
+
+pod=$(sudo microk8s kubectl get pods | grep postgres) 
+
+sudo microk8s kubectl exec -it $pod -- pg_dump "$1" -U user --column-inserts --data-only
